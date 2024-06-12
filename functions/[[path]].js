e93f9aa63ea16019061f106e3e97b23e18366c16
Ryan Florence <rpflorence@gmail.com>
2024-06-12 13:05:41 -0600

    cool

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,4 +1,4 @@
 export async function onRequest(context) {
   let customers = await context.env.DB.prepare("SELECT * FROM Customers").all();
-  return Response.json(customers);
+  return Response.json(customers.results);
 }
